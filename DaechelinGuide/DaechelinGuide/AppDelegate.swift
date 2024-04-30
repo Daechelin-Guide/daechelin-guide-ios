550bc23d918ab62624f1a2718a2de05ee43b639c
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-04-30 16:57:41 +0900

    Add: Project file

---
+++ DaechelinGuide/DaechelinGuide/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DaechelinGuide
+//
+//  Created by 이민규 on 4/30/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
---
+++ DaechelinGuide/DaechelinGuide/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DaechelinGuide
+//
+//  Created by 이민규 on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
